c31adb3f1b6dea055bd3605dccbce15c23e5913f
devisrael <devisrael560@gmail.com>
2025-06-11 10:58:44 -0300

    chore: update schemas with validation rules and error messages

--- schemas/RequestSchemas.ts
+++ schemas/RequestSchemas.ts
@@ -1,4 +1,3 @@
-
 import { z } from 'zod';
 
 export const TaskSchema = z.object({
@@ -13,20 +12,19 @@ export const TaskTypeSchema = z.object({
   description: z.string(),
 });
 
-
 export const AnimalSchema = z.object({
-  name: z.string(),
-  specie: z.string()
-})
+  name: z.string().min(3, "The animal's name must be at least 3 characters long."),
+  specie: z.string().min(2, "The species must be at least 2 characters long.")
+});
 
 export const UserSchema = z.object({
   username: z.string(),
-  password: z.string(),
-  role: z.string().transform(val => val.toUpperCase()),
-})
+  password: z.string().min(6, "The password must be at least 6 characters long."),
+  role: z.string().min(4).max(5).transform(val => val.toUpperCase()),
+});
 
 export const RegisterSchemma = z.object({
   username: z.string(),
   role: z.string(),
   password: z.string()
-})
+});
--- schemas/formSchema.ts
+++ schemas/formSchema.ts
@@ -1,15 +1,27 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-    username: z.string().min(3, "Seu nome deve conter no minimo 3 letras."),
-    password: z.string().min(6, "Sua senha deve ter no minimo 6 caracteres."),
-    role: z.string().min(4).max(5),
-})
+  username: z
+    .string()
+    .min(3, "Your name must be at least 3 characters long."),
+  password: z
+    .string()
+    .min(6, "Your password must be at least 6 characters long."),
+  role: z
+    .string()
+    .min(4)
+    .max(5),
+});
 
 export const loginSchema = z.object({
-    username: z.string().min(3, "Seu nome deve conter no minimo 3 letras.").nonempty("Nome do usuário é obrigatório"),
-    password: z.string().min(6, "Senha é obrigátoria.")
-})
+  username: z
+    .string()
+    .min(3, "Your name must be at least 3 characters long.")
+    .nonempty("Username is required."),
+  password: z
+    .string()
+    .min(6, "Password is required."),
+});
 
 export type RegisterForm = typeof registerSchema;
 export type LoginForm = typeof loginSchema;
