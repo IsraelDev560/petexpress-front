d34b2df7d1055259adf62cb9a0578f7cb3e4df5e
devisrael <devisrael560@gmail.com>
2025-06-11 11:00:47 -0300

    feat: update API_URL and NEXT_PUBLIC_API_FRONT based on NODE_ENV

--- lib/ApiUrl.ts
+++ lib/ApiUrl.ts
@@ -1,2 +1,9 @@
-export const API_URL = process.env.API_URL;
-export const NEXT_PUBLIC_API_FRONT = process.env.NEXT_PUBLIC_API_FRONT;
+const isProd = process.env.NODE_ENV === "production";
+
+export const API_URL = isProd
+  ? process.env.API_URL
+  : process.env.API_URL_DEV;
+
+export const NEXT_PUBLIC_API_FRONT = isProd
+  ? process.env.NEXT_PUBLIC_API_FRONT
+  : process.env.NEXT_PUBLIC_API_FRONT_DEV;
