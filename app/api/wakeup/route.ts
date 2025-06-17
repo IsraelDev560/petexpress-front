7a848fd92dfd97011a0e90e4849ba12f86407aba
devisrael <devisrael560@gmail.com>
2025-06-17 12:53:46 -0300

    feat: change GET to POST method in /api/wakeup/route.ts for login request

--- app/api/wakeup/route.ts
+++ app/api/wakeup/route.ts
@@ -1,8 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
-    const res = await fetch(`${process.env.API_URL}`);
+    const res = await fetch(`${process.env.API_URL}/auth/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: "Teste", password: "TEste123"})
+    });
     if (!res.ok) throw new Error("Falha no ping");
     return new NextResponse(JSON.stringify({ message: "Acordou com sucesso" }), { status: 200 });
   } catch (err: any) {
